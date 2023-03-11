c657ca67158945a4f96f887c5ba6b06921e6f7c4
Moz <contact@ether-source.fr>
2023-03-11 15:19:15 +0100

    better to dump with fd open & read from env some stuff

--- harvest_wallet.py
+++ harvest_wallet.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 import time
+import os
 
 import requests  # Thoses packages need to be installed
 import yaml
@@ -8,8 +9,8 @@ import yaml
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level='INFO')  # Just a looger
 
 #  CONFIG PART
-wallet_who_collect = ''  # put the address who should collectd
-wallet_password = ''  # put your password prompt
+wallet_who_collect = os.getenv('NIBID_ADDR')  # put the address who should collectd
+wallet_password = os.getenv('NIBID_ADD_PASSWORD')  # put your password prompt
 wallet_to_harvest_pattern = 'harvest'  # wallet pattern to harvest
 wallet_minimum_harvest = 25000  # minimal unibi to get
 file_to_read = 'sample.yml'  # output file generated from nibib keys list > sample.yml
