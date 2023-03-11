33d8b4881812144f1102c27b029ea831a1b1a1bc
Moz <contact@ether-source.fr>
2023-03-11 15:11:04 +0100

    update with current version

--- create_harvest_script.py
+++ create_harvest_script.py
@@ -1,25 +1,20 @@
-    import yaml
+import yaml
 
-    file_to_read = "sample.yml"  # output file generated from nibib keys list > sample.yml
-    curl_pattern = "curl_background.bash"
-    dest = "sample/shell_"
+stream = open('harvest_key.yml', 'r')
+obj = yaml.safe_load(stream)
+addresses = []
+for x, line in enumerate(obj):
+    addresses.append(line['address'])
+    if x % 10 == 0 and x > 0:
+        a = open("pattern.sh", "r")
+        d = open("sample/shell_{}.sh".format(x), "w")
+        for i, sample in enumerate(a):
+            d.write(sample)
+            if i == 2:
+                for address in addresses:
+                    d.write("{}\n".format(address))
+        addresses = []
+        d.close()
+        a.close()
 
-    stream = open(file_to_read, 'r')
-    obj = yaml.safe_load(stream)
-    addresses = []
-    for x, line in enumerate(obj):
-        addresses.append(line['address'])
-        if x % 10 == 0 and x > 0:
-            a = open(curl_pattern, "r")
-            d = open("{}{}.sh".format(dest, x), "w")
-            for i, sample in enumerate(a):
-                d.write(sample)
-                if i == 2:
-                    for address in addresses:
-                        d.write('{}\n'.format(address))
-            addresses = []
-            d.close()
-            a.close()
-
-    stream.close()
-
+stream.close()
