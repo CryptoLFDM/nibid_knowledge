f8c5259f82cbce8144b99fa507fad28e7d5c8b7b
Douceur <contact@ether-source.fr>
2023-03-11 15:54:33 +0100

    fix harvest_generator

--- create_harvest_script.py
+++ create_harvest_script.py
@@ -1,20 +1,25 @@
 import yaml
 
-stream = open('harvest_key.yml', 'r')
+stream = open('wallet.yml', 'r')
 obj = yaml.safe_load(stream)
 addresses = []
+
+def add_address(x):
+    a = open("pattern.sh", "r")
+    d = open("sample/shell_{}.sh".format(x), "w")
+    for i, sample in enumerate(a):
+        d.write(sample)
+        if i == 2:
+            for address in addresses:
+                d.write("{}\n".format(address))
+    d.close()
+    a.close()
+
 for x, line in enumerate(obj):
     addresses.append(line['address'])
     if x % 10 == 0 and x > 0:
-        a = open("pattern.sh", "r")
-        d = open("sample/shell_{}.sh".format(x), "w")
-        for i, sample in enumerate(a):
-            d.write(sample)
-            if i == 2:
-                for address in addresses:
-                    d.write("{}\n".format(address))
+        add_address(x)
         addresses = []
-        d.close()
-        a.close()
+add_address(x)
 
 stream.close()
---
+++ pattern.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+myArray=(
+
+)
+
+for str in "${myArray[@]}"; do
+    curl -X POST -s -d '{"address": "'"$str"'", "coins": ["110000000unibi","100000000unusd","100000000uusdt"]}' "https://faucet.itn-1.nibiru.fi/" &
+done
