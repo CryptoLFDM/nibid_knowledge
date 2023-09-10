c50f50074f9952e8b666b0121be0e37ae45c99aa
Mozquito <chimera@chimera-mu.mythologic.fr>
2023-09-10 19:37:20 +0000

    remove old stuff and switch testnet

--- curl_generator/pattern.sh
+++ curl_generator/pattern.sh
@@ -5,5 +5,5 @@ myArray=(
 )
 
 for str in "${myArray[@]}"; do
-    curl -X POST -s -d '{"address": "'"$str"'", "coins": ["110000000unibi","100000000unusd","100000000uusdt"]}' "https://faucet.itn-1.nibiru.fi/" &
+    curl -X POST -s -d '{"address": "'"$str"'", "coins": ["110000000unibi","100000000unusd","100000000uusdt"]}' "https://faucet.itn-2.nibiru.fi/" &
 done
